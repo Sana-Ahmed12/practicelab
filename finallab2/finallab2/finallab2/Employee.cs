77e7bd4140423f608548f7024a166c90f7fa0087
Sana-Ahmed12 <93635146+Sana-Ahmed12@users.noreply.github.com>
2021-11-30 01:07:52 -0800

    Add files via upload

---
+++ finallab2/finallab2/finallab2/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab2
+{
+    public class Employee
+    {
+        public int EmployeeID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int EmployeeName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int EmployeeType
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int HoursWorkedInThatMonth
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        public void calculatePay()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
